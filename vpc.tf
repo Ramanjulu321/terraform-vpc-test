d1662e965f936db5653854d289ce3d37903eb9b1
Ramanji <ramanjulu7890@gmail.com>
2024-05-27 14:23:21 +0530

    good efforts

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    source = "../terraform-aws-vpc"
+    source =  "git::https://github.com/Ramanjulu321/terraform-aws-vpc.git?ref=main"
     project_name = var.project_name
     common_tags = var.common_tags
     public_subnet_cidrs = var.public_subnet_cidrs 
